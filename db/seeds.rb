65dd93d61755112033940aa4d300b265138426bb
edwinlin <edwinlin@gmail.com>
2019-02-17 04:55:03 -0500

    added logic to record not found after deleting booking and then pressing back page button. Code changed in bookings_controller#set_booking and #show. Users show view moved flash notice and flash error.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@ User.create(name: "Cassidy Samelian", email: "cass@cass.com", password: "1")
 
 50.times do Listing.create(location: Faker::Address.full_address, user_id: Faker::Number.between(1, 50)) end
 
-# 25.times do Booking.create(user_id: Faker::Number.between(1, 50), start_date: Faker::Date.forward(60), end_date: Faker::Date.forward(70), listing_id: Faker::Number.between(1, 100)) end
+# 10.times do Booking.create(user_id: Faker::Number.between(1, 50), start_date: Faker::Date.forward(60), end_date: Faker::Date.forward(70), listing_id: Faker::Number.between(1, 100)) end
